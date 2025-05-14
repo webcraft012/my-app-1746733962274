2d25424d6d29d3500e94325c763b61290e5e5072
CodeSandbox <support@codesandbox.io>
2025-05-14 21:32:01 +0000

    Apply code edit to file: components/Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,16 @@ const Header = () => {
       <div className="container mx-auto flex justify-between items-center">
         <h1 className="text-2xl font-bold">ParaShop</h1>
         {/* Future: Search and Cart Icons */}
-        <div>
+        <h1 className="text-2xl font-bold">ParaShop</h1>
+        <div className="flex items-center space-x-4">
+          <input
+            type="text"
+            placeholder="Search products..."
+            className="px-3 py-1 rounded-md border"
+          />
+          {/* Future: Cart Icon */}
+        </div>
+      </div>
           {/* Icons will go here */}
         </div>
       </div>
