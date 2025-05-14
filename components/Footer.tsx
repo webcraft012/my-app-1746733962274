8a20c835f9fedb2b79ed2194b4a8ebb124b6891b
CodeSandbox <support@codesandbox.io>
2025-05-14 21:30:45 +0000

    Create file: components/Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-secondary text-secondary-foreground p-4 mt-8">
+      <div className="container mx-auto text-center text-sm">
+        © 2023 ParaShop. All rights reserved.
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
