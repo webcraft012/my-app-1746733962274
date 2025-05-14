aafba1b5e80f59e17b6d516ba2b808cd8533cef2
CodeSandbox <support@codesandbox.io>
2025-05-14 21:31:40 +0000

    Create file: app/checkout/page.tsx

---
+++ app/checkout/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import React from 'react';
+
+const CheckoutPage = () => {
+  return (
+    <div className="container mx-auto py-8">
+      <h2 className="text-3xl font-bold mb-6">Checkout</h2>
+      {/* Future: Shipping, Billing, Payment sections */}
+    </div>
+  );
+};
+
+export default CheckoutPage;
