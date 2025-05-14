553dbae681f06633f1e16533f19201f1875bce19
CodeSandbox <support@codesandbox.io>
2025-05-14 21:31:14 +0000

    Create file: app/products/page.tsx

---
+++ app/products/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import React from 'react';
+
+const ProductsPage = () => {
+  return (
+    <div className="container mx-auto py-8">
+      <h2 className="text-3xl font-bold mb-6">All Products</h2>
+      {/* Future: Product listing */}
+    </div>
+  );
+};
+
+export default ProductsPage;
